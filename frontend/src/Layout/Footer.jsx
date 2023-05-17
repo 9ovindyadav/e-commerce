405d95da11d6c109e13f9f3e6aa20c477d5e4069
Govind <govindsvyadav@gmail.com>
2023-05-17 19:25:25 +0530

    frontend: product card, footer

--- frontend/src/Layout/Footer.jsx
+++ frontend/src/Layout/Footer.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom"
+import Logo from "../assets/images/logo.png"
+import "../sass/layout.scss"
+import {RiVisaLine} from "react-icons/ri"
+import {FaAmazonPay, FaCcMastercard} from "react-icons/fa"
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="brand">
+      <Link to={"/"} className="logo">
+        <img src={Logo} alt="red apple as logo of the brand" />
+        <span>Shopcart</span>
+      </Link>
+      <p>Shopping is a bit of relaxing hobby for me, which is somethimes troubling for the bank balance.</p>
+      <h3>Accepted payments</h3>
+      <div className="payments">
+        <RiVisaLine/>
+        <FaCcMastercard />
+        <FaAmazonPay/>
+      </div>
+      </div>
+    </footer>
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,35 +5,73 @@ const category = [
 {
   title:"Sport's wear",
   path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
-},
-{
-  title:"Sport's wear",
-  path: "/sale"
 }
+]
+
+const products = [
+  {
+    ProID:"hjadhgamga78135825282462",
+    ProImg:"https://cdn.shopify.com/s/files/1/1002/7150/products/New-Mockups---no-hanger---TShirt-Yellow.jpg?v=1639657077",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  },
+  {
+    ProID:"hjadhgamga781358252824634",
+    ProImg:"https://m.media-amazon.com/images/I/61iKv6-unXL._UY879_.jpg",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  },
+  {
+    ProID:"hjadhgamga781358252824634",
+    ProImg:"https://m.media-amazon.com/images/I/61iKv6-unXL._UY879_.jpg",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  },
+  {
+    ProID:"hjadhgamga781358252824634",
+    ProImg:"https://m.media-amazon.com/images/I/61iKv6-unXL._UY879_.jpg",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  },
+  {
+    ProID:"hjadhgamga781358252824634",
+    ProImg:"https://m.media-amazon.com/images/I/61iKv6-unXL._UY879_.jpg",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  },
+  {
+    ProID:"hjadhgamga781358252824634",
+    ProImg:"https://m.media-amazon.com/images/I/61iKv6-unXL._UY879_.jpg",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  },
+  {
+    ProID:"hjadhgamga781358252824634",
+    ProImg:"https://m.media-amazon.com/images/I/61iKv6-unXL._UY879_.jpg",
+    ProName:"Home brand",
+    ProPrice:"$90.00",
+    ProDesc: "available in 5 different colors",
+    ProRating: 5,
+    ProReviews: 200
+  }
 ]
 
 export const Home = () => {
@@ -49,7 +87,8 @@ export const Home = () => {
           </div>
         </div>
       </section>
-     <section className="category">
+
+     <section className="home-section">
       <h1>Shop Our Top Categories</h1>
       <div className="roller">
         {
@@ -63,6 +102,45 @@ export const Home = () => {
         }
       </div>
      </section>
+
+     <section className="home-section">
+      <h1>Today's best deal for you</h1>
+      <div className="roller">
+        {
+          products.map(({ProID,ProImg,ProName,ProPrice,ProDesc,ProRating,ProReviews})=>(
+            
+          
+            <ProductCard key={ProID} ProID={ProID} ProImg={ProImg} 
+                     ProName={ProName} 
+                     ProPrice={ProPrice} 
+                     ProDesc={ProDesc} 
+                     ProRating={ProRating} 
+                     ProReviews={ProReviews} />
+      
+          ))
+        }
+      </div>
+     </section>
     </main>
+  )
+}
+
+const ProductCard = ({ProID,ProImg,ProName,ProPrice,ProDesc,ProRating,ProReviews}) => {
+
+  return(
+    <div className="product-card">
+      <Link style={{margin:0,padding:0,borderRadius:0,background:"none"}} to={`/product/${ProID}`}>
+      <div className="img">
+        <img src={ProImg} alt={ProName} />
+      </div>
+      <div className="title">
+      <h2>{ProName}</h2>
+      <h2>{ProPrice}</h2>
+      </div>
+      <p>{ProDesc}</p>
+      <p>{ProRating} ({ProReviews})</p>
+      </Link>
+      <button className="btn-transparent">Add to Cart</button>
+    </div>
   )
 }
