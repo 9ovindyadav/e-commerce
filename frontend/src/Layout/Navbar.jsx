03b74e088afbfc8fac825a2c5b027961548a19d9
Govind <govindsvyadav@gmail.com>
2023-05-17 15:55:59 +0530

    frontend: navbar, homepage

--- frontend/src/Layout/Navbar.jsx
+++ frontend/src/Layout/Navbar.jsx
@@ -1,5 +1,28 @@
+import Logo from "../../src/assets/images/logo.png"
+import "../sass/layout.scss";
+import {BsChevronDown, BsPersonFill, BsCart3} from "react-icons/bs"
+import {BiSearch} from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <Link to={"/"} className="logo">
+        <img src={Logo} alt="red apple as logo of the brand" />
+        <span>Shopcart</span>
+      </Link>
+      <ul>
+        <li><Link to="/category">Category</Link> <BsChevronDown/> </li>
+        <li><Link to="/">Men's</Link></li>
+        <li><Link to="/">Women's</Link></li>
+        <li><Link to="/sale">Sale</Link></li>
+      </ul>
+      <div className="search">
+      <input type="text" placeholder="Search product" /> <BiSearch/>
+      </div>
+      <Link to="/account"><BsPersonFill/> Account</Link>
+      <Link to="/cart"><BsCart3/> Cart</Link>
+    </nav>
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,5 +1,68 @@
+import { Link } from "react-router-dom"
+import "../sass/home.scss"
+
+const category = [
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+},
+{
+  title:"Sport's wear",
+  path: "/sale"
+}
+]
+
 export const Home = () => {
+
   return (
-    <div>Home</div>
+    <main>
+      <section>
+        <div className="home-top">
+          <div className="text">
+          <h1>Shopping And Department Store</h1>
+          <p>Shopping is a bit of relaxing hobby for me, which is somethimes troubling for the bank balance.</p>
+          <Link to={"/sale"}><button>Buy Now</button></Link>
+          </div>
+        </div>
+      </section>
+     <section className="category">
+      <h1>Shop Our Top Categories</h1>
+      <div className="roller">
+        {
+          category.map((item)=>(
+           
+               <Link to={item.path} key={item.i}>
+               <h2>{item.title}</h2>
+               </Link>
+    
+          ))
+        }
+      </div>
+     </section>
+    </main>
   )
 }
