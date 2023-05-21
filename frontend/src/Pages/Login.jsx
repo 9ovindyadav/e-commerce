58829ee8ce99253ad843d2b796109e28265ef8d5
Govind <govindsvyadav@gmail.com>
2023-05-21 08:54:14 +0530

    auth fields

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom"
 import "../sass/login.scss"
+import { useState } from "react";
 
 export const Login = () => {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(email,password);
+    }
   return (
 <div className="container">
-<form action="">
+<form action="" onSubmit={submitHandler}>
     <h1>Login</h1>
-    <input type="text" placeholder="Email"/>
-    <input type="password" placeholder="Password" />
+    <input type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Email"/>
+    <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
     <div>
         <Link to={"/forget-password"}>Forget password</Link>
         <Link to={"/reset-password"}>Reset password</Link>
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom"
 import "../sass/login.scss"
+import { useState } from "react"
 
 
 export const Register = () => {
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(name,email,password);
+    }
   return (
     <div className="container">
-<form action="">
+<form action="post" onSubmit={submitHandler}>
     <h1>Register</h1>
-    <input type="text" placeholder="Full name" />
-    <input type="email" placeholder="Email"/>
-    <input type="password" placeholder="Password" />
+    <input type="text" onChange={(e)=>setName(e.target.value)} placeholder="Full name" />
+    <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder="Email"/>
+    <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
     <button className="btn-register" type="submit">Submit</button>
     <label>Already registered ? <Link to="/login">Login</Link></label>
 </form>
