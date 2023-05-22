a1ddc01eb4d1e734267cbf4a17292d53542c0b36
Govind <govindsvyadav@gmail.com>
2023-05-22 21:23:25 +0530

    mobile-menu

--- frontend/src/Layout/Navbar.jsx
+++ frontend/src/Layout/Navbar.jsx
@@ -3,8 +3,9 @@ import "../sass/layout.scss";
 import {BsChevronDown, BsPersonFill, BsCart3} from "react-icons/bs"
 import {BiSearch, BiLogIn} from "react-icons/bi";
 import {FiMenu} from "react-icons/fi"
+import {AiOutlineClose} from "react-icons/ai"
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const user = {
   // id:"4726943865"
@@ -19,6 +20,36 @@ export const Navbar = () => {
       setShow(false);
     }
   }
+
+  const mySidenav = useRef();
+const pannelDisplay1 = useRef();
+const pannelDisplay2 = useRef();
+
+  const openMenu = ()=>{
+    mySidenav.current.style.width = "70%" ;
+  }
+  const closeMenu = ()=>{
+    mySidenav.current.style.width = "0" ;
+  }
+   
+  const pannelDisplayHandler1 = ()=>{
+   
+  if(pannelDisplay1.current.style.display === "block"){
+    pannelDisplay1.current.style.display = "none" ;
+  }else{
+    pannelDisplay1.current.style.display = "block" ;
+  }
+
+  }
+  
+const pannelDisplayHandler2 = ()=>{
+
+    if(pannelDisplay2.current.style.display === "block"){
+        pannelDisplay2.current.style.display = "none" ;
+      }else{
+        pannelDisplay2.current.style.display = "block" ;
+      }
+}
 
   return (
     <nav>
@@ -46,7 +77,32 @@ export const Navbar = () => {
         </div> : <Link to={"/login"}><BiLogIn/> Login</Link>
       }
       <Link to="/cart"><BsCart3/> Cart</Link>
-      <FiMenu className="mobile-menu"/>
+
+      <FiMenu className="mobile-menu" onClick={openMenu}/>
+      <div ref={mySidenav} class="sidenav">
+            <span href="javascript:void(0)" class="closebtn" onClick={closeMenu}><AiOutlineClose/></span>
+            <div className="nav1">
+            <a href="#" onClick={pannelDisplayHandler1}>Features <BsChevronDown/></a>
+            <div ref={pannelDisplay1} className="panel">
+                <a href="#">Todo List</a>
+                <a href="#">Calender</a>
+                <a href="#">Reminder</a>
+                <a href="#">Planning</a>
+            </div>
+            <a href="#" onClick={pannelDisplayHandler2}>Company </a>
+            <div ref={pannelDisplay2} className="panel">
+                <a href="#">History</a>
+                <a href="#">Our team</a>
+                <a href="#">Blog</a>
+            </div>
+            <a href="#">Carears</a>
+            <a href="#">About</a>
+            </div>
+            <div className="nav2">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+             </div>
+       </div>
     </nav>
   )
 }
